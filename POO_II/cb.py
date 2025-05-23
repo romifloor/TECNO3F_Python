06e6c9f5f847b154fc20d90e043fcf4953807826
Romina Florencia Ramos <romi859@gmail.com>
2025-05-23 15:08:52 -0300

    Cuenta bancaria

---
+++ POO_II/cb.py
@@ -0,0 +1,33 @@
+from datetime import date, datetime
+
+class CuentaBancaria:
+    def __init__(self,nombre_titular,dni_titular, fecha_nacimiento, saldo=0):
+        self._nombre_titular = nombre_titular       #atributo privado
+        self._dni_titular = dni_titular             #atributo privado
+        self._fecha_nacimiento = datetime.strptime(fecha_nacimiento, '%Y/%m/%d').date()
+        self._saldo = saldo                         #atributo privado
+
+    def obtener_saldo(self):
+        return self._saldo
+    
+    def depositar(self,monto):
+        if monto > 0:
+            self._saldo += monto
+            print(f"Se ha depositado {monto} a la cuenta de {self._nombre_titular}, su saldo es de: {self.obtener_saldo()}")
+        else:
+            print("El monto a depositar debeser mayor a 0")
+
+    def extraer(self,monto):
+        if monto <= self.obtener_saldo():
+            self._saldo -= monto
+            print(f"Se ha extraido {monto} de la cuenta de {self._nombre_titular}, su saldo acutal es de: {self.obtener_saldo()}")
+        else:
+            print("No posee saldo suficiente para esta operación")
+
+    def _caclular_edad(self):
+        fecha_actual = date.today()
+        edad = fecha_actual - self._fecha_nacimiento
+        return edad.days // 365
+    
+    def obtener_edad(self):
+        return self._caclular_edad()
